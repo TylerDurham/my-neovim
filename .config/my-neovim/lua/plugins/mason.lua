6a1152a519c335fc28371611c63fee4547305506
D. Tyler Durham <TylerDurham@users.noreply.github.com>
2026-05-07 21:17:07 -0500

    feat(mason, blink): basic lsp configuration.

---
+++ .config/my-neovim/lua/plugins/mason.lua
@@ -0,0 +1,30 @@
+
+return {
+  {
+    "mason-org/mason.nvim",
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      "neovim/nvim-lspconfig"
+    },
+    config = function()
+      require("mason").setup({})
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+        }
+      })
+
+      local lsp_config = require("lspconfig")
+
+      local mason_tools = require('mason-tool-installer')
+      mason_tools.setup({
+        ensure_installed = {
+          "shfmt",
+          "stylua",
+        }
+      })
+    end
+  },
+}
