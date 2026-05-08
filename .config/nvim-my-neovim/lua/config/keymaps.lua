f5280bc47d762f171c657226da4c25d98f5d501a
D. Tyler Durham <TylerDurham@users.noreply.github.com>
2026-05-07 19:57:10 -0500

    feat:initial commit.

---
+++ .config/nvim-my-neovim/lua/config/keymaps.lua
@@ -0,0 +1,43 @@
+-- My preferred mappings
+map = vim.keymap.set
+
+-- Escape
+map("i", "jj", "<ESC>", { desc="Escape from mode 'INSERT'" })
+
+-- Keep cursor in place when yanking in visual mode
+map("v", "y", "ygv<Esc>", { desc = "Yank and keep cursor" })
+
+-- Paste without overwriting the unnamed register (VERY useful)
+-- When you yank something, then visually select and paste over text,
+-- the yanked text gets replaced by what you just deleted. This fixes that:
+map("v", "p", '"_dP', { desc = "Paste without yanking selection" })
+
+-- Yank to system clipboard explicitly (even without unnamedplus)
+map({"n", "v"}, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+map("n", "<leader>Y", '"+Y',        { desc = "Yank line to system clipboard" })
+
+-- Delete without yanking (black hole register)
+map({"n", "v"}, "<leader>d", '"_d', { desc = "Delete without yanking" })
+
+-- Paste from system clipboard explicitly
+map({"n", "v"}, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+map({"n", "v"}, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+
+-- Line numbers
+-- Toggle between number modes:
+-- 1. relative + absolute (hybrid) -- normal editing
+-- 2. absolute only               -- good for copying with mouse
+-- 3. no numbers                  -- clean/distraction free
+
+local function toggle_line_numbers()
+    if vim.opt.relativenumber:get() then
+        vim.opt.relativenumber = false
+    elseif vim.opt.number:get() then
+        vim.opt.number = false
+    else
+        vim.opt.number = true
+        vim.opt.relativenumber = true
+    end
+end
+
+vim.keymap.set("n", "<leader>tn", toggle_line_numbers, { desc = "Toggle Line Numbers" })
---
+++ .config/nvim-my-neovim/lua/config/options.lua
@@ -0,0 +1,33 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = " "       -- Space as leader (most popular choice)
+vim.g.maplocalleader = " "  -- Backslash as local leader (used by some plugins)
+
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true          -- Show absolute line numbers
+opt.relativenumber = true  -- Show relative line numbers (great for motions)
+
+-- Indentation
+opt.tabstop = 2            -- Number of spaces a tab counts for
+opt.shiftwidth = 2         -- Spaces used for each step of (auto)indent
+opt.expandtab = true       -- Convert tabs to spaces
+opt.autoindent = true      -- Copy indent from current line when starting a new one
+opt.smartindent = true     -- Smarter autoindenting (e.g. after { in C-like files)
+
+-- Wrapping
+opt.wrap = false           -- Disable line wrapping (keeps code clean)
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"  -- use system clipboard for all operations
+
+-- Apperance
+opt.termguicolors = true   -- Enable 24-bit RGB colors in the terminal
+opt.cursorline = true      -- Highlight the line the cursor is on
+opt.signcolumn = "yes"     -- Always show the sign column (prevents layout shifts)
+opt.colorcolumn = "100"     -- Visual ruler at column 80
+opt.scrolloff = 8          -- Keep 8 lines visible above/below cursor
+opt.sidescrolloff = 8      -- Keep 8 columns visible left/right of cursor
+opt.showmode = false       -- Don't show -- INSERT -- (use a statusline plugin instead)
