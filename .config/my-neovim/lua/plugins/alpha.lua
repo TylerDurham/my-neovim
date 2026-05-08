ca1c2b684403262bc8c4745645d90a2bee0ff4c2
D. Tyler Durham <TylerDurham@noreply.users.github.com>
2026-05-08 17:55:44 -0500

    feat(colorscheme): add M.current() and show active theme on dashboard

--- .config/my-neovim/lua/plugins/alpha.lua
+++ .config/my-neovim/lua/plugins/alpha.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
-
     local opts = require("config.settings")
+    local colorscheme = require("config.colorscheme").current()
     
     -- ASCII art header
     dashboard.section.header.val = vim.split(opts.alpha.header, "\n", { trimempty = true })
@@ -17,7 +17,7 @@ return {
       dashboard.button("g", "  Find text",       "<cmd>Telescope live_grep<CR>"),
       dashboard.button("n", "  Config",          "<cmd>e $MYVIMRC<CR>"),
       dashboard.button("r", "  Recent files",    "<cmd>Telescope oldfiles<CR>"),
-      dashboard.button("t", "  Change theme",    "<cmd>ThemeSelect<CR>"),
+      dashboard.button("t", "  Change theme (current: " .. colorscheme .. ")",    "<cmd>ThemeSelect<CR>"),
       dashboard.button("q", "  Quit",            "<cmd>qa<CR>"),
     }
 
