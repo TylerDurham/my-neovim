d9f4a8bfc1ca04e6927f288e4914e56ae391ff0a
D. Tyler Durham <TylerDurham@noreply.users.github.com>
2026-05-08 22:53:07 -0500

    refactor(nvim): extract lazy bootstrap into config/lazy.lua
    Moves the lazy.nvim bootstrap block out of init.lua into a dedicated
    config/lazy.lua module, keeping init.lua as a clean entry point that
    just sequences the four top-level concerns: options, keymaps, plugins,
    and colorscheme.

--- .config/my-neovim/init.lua
+++ .config/my-neovim/init.lua
@@ -1,31 +1,11 @@
+-- Options
 require("config.options")
+
+-- Keymaps
 require("config.keymaps")
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
-    "git", "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    lazyrepo,
-    lazypath
-  })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to continue..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
+-- Lazy/plugins
+require("config.lazy")
 
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-
--- Colorscheme
+-- Custom colorscheme/theme switcher
 require("config.colorscheme").load()
